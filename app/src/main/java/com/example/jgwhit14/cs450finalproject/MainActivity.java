1474aef506392479577ce1333ceac1bf63418a35
gordon636 <gordonwhite636@gmail.com>
2018-10-25 19:39:46 -0400

    Small Init Stuff

--- app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
+++ app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.jgwhit14.cs450finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signupBtn, loginBtn;
+    private String loginEmail, loginPW, signUpEmail, signUpName, signUpPW1, signUpPW2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        signupBtn = findViewById(R.id.signUpBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+
     }
 }
