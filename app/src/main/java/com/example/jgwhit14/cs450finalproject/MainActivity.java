6fe29cabb2c14d408a42fa8f8d92485f8e5aa7a5
gordon636 <gordonwhite636@gmail.com>
2018-10-25 19:44:28 -0400

    Text Views to Edit Text

--- app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
+++ app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.jgwhit14.cs450finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signupBtn, loginBtn;
-    private String loginEmail, loginPW, signUpEmail, signUpName, signUpPW1, signUpPW2;
+    private EditText loginEmail, loginPW, signUpEmail, signUpName, signUpPW1, signUpPW2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
         signupBtn = findViewById(R.id.signUpBtn);
         loginBtn = findViewById(R.id.loginBtn);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPW = findViewById(R.id.loginPW);
+        signUpEmail = findViewById(R.id.signUpEmail);
+        signUpName = findViewById(R.id.signUpName);
+
+
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
