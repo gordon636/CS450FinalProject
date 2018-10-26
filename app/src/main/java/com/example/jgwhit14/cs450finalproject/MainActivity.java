159cde2305fc2eb8f045dd89e685883a4cf5083b
wazzzaonthebeat <42841110+wazzzaonthebeat@users.noreply.github.com>
2018-10-25 20:02:47 -0400

    Hi Gordon

--- app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
+++ app/src/main/java/com/example/jgwhit14/cs450finalproject/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.jgwhit14.cs450finalproject;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signupBtn, loginBtn;
     private EditText loginEmail, loginPW, signUpEmail, signUpName, signUpPW1, signUpPW2;
-
+    private  FirebaseDatabase database;
+    private String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
 
         signupBtn = findViewById(R.id.signUpBtn);
         loginBtn = findViewById(R.id.loginBtn);
@@ -24,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         signUpName = findViewById(R.id.signUpName);
 
 
+        database  = FirebaseDatabase.getInstance();
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +47,41 @@ public class MainActivity extends AppCompatActivity {
                 String email = signUpEmail.getText().toString();
                 String password = signUpEmail.getText().toString();
 
+
+            }
+        });
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //save variables entered to strings
+                String email = loginEmail.getText().toString();
+                String password = loginPW.getText().toString();
+
+                //match the password to
+                DatabaseReference loginRef = database.getReference("Users");
+                loginRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        Iterable<DataSnapshot> names = dataSnapshot.getChildren();
+
+                        for (DataSnapshot name:names){
+
+                            String Name = name.getValue(String.class);
+                            Log.i(TAG,Name);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
             }
         });
