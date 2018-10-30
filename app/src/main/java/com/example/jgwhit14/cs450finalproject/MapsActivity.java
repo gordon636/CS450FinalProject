657a6a0e1e51a49ddaaea249ac1e12c644d7d6e0
wazzzaonthebeat <42841110+wazzzaonthebeat@users.noreply.github.com>
2018-10-30 10:58:28 -0400

    Fixed alot of shit - Wazzza

--- app/src/main/java/com/example/jgwhit14/cs450finalproject/MapsActivity.java
+++ app/src/main/java/com/example/jgwhit14/cs450finalproject/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.jgwhit14.cs450finalproject;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -8,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -171,4 +173,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             });
         }
     }
+
+    public void locations (View view){
+
+        Intent intent = new Intent(this, MyLocations.class);
+        startActivity(intent);
+    }
+
+    public void friends (View view){
+
+        Intent intent = new Intent(this, Friends.class);
+        startActivity(intent);
+    }
+
+
 }
